7cc346b6d4d16dd7bad7f07fb99a9c92666905e2
Carl <carlgarces7@gmail.com>
2022-02-25 18:54:41 +0800

    End Vid 1

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,10 @@ class _MyHomePageState extends State<MyHomePage> {
           title: const Text("ELEC 2B REVIEW"),
         ),
         body: Column(
-          children: const [NumberHolder(content: "one")],
+          children: const [
+            NumberHolder(content: "one"),
+            IncrementalNumberHolder(),
+          ],
         ));
   }
 }
@@ -51,23 +54,57 @@ class NumberHolder extends StatelessWidget {
     return Container(
         margin: const EdgeInsets.symmetric(vertical: 0),
         padding: const EdgeInsets.all(4),
+        constraints: const BoxConstraints(minHeight: 60),
         width: double.infinity,
         color: Colors.orangeAccent,
         child: Text("$content", textAlign: TextAlign.center));
   }
 }
 
-class IncrementalNumberHolder extends StatelessWidget {
+class IncrementalNumberHolder extends StatefulWidget {
   final int startval;
   const IncrementalNumberHolder({Key? key, this.startval = 0})
       : super(key: key);
+
+  @override
+  State<IncrementalNumberHolder> createState() =>
+      _IncrementalNumberHolderState();
+}
+
+class _IncrementalNumberHolderState extends State<IncrementalNumberHolder> {
+  @override
+  void initState() {
+    currentVal = widget.startval;
+    super.initState();
+  }
+
+  late int currentVal;
   @override
   Widget build(BuildContext context) {
     return Container(
-        margin: const EdgeInsets.symmetric(vertical: 0),
-        padding: const EdgeInsets.all(4),
-        width: double.infinity,
-        color: Colors.orangeAccent,
-        child: Text("$startval", textAlign: TextAlign.center));
+      margin: const EdgeInsets.symmetric(vertical: 0),
+      padding: const EdgeInsets.all(4),
+      width: double.infinity,
+      color: Colors.orangeAccent,
+      child: Row(
+        children: [
+          IconButton(
+              onPressed: () {
+                setState(() {
+                  currentVal--;
+                });
+              },
+              icon: const Icon(Icons.chevron_left)),
+          Expanded(child: Text("$currentVal", textAlign: TextAlign.center)),
+          IconButton(
+              onPressed: () {
+                setState(() {
+                  currentVal++;
+                });
+              },
+              icon: const Icon(Icons.chevron_right)),
+        ],
+      ),
+    );
   }
 }
