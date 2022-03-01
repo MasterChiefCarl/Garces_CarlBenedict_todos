5b3656d552e833978c3d039a3b938d7404164589
Carl <carlgarces7@gmail.com>
2022-03-01 13:52:30 +0800

    Creating the_great_adventure

---
+++ the_great_adventure/lib/main.dart
@@ -0,0 +1,24 @@
+
+import '/screens/start_screen.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Review App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const StartScreen(),
+    );
+  }
+}
+
